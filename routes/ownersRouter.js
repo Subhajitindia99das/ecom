a2305cfd1f41738e6a772d49b90131174c78c4ee
Subhojit Das <subhojitdas@Subhojits-MacBook-Air.local>
2025-12-08 19:12:48 +0530

    The owner router has been created...

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const ownerModel = require("../models/owner-model")
+
+if(process.env.NODE_ENV==="development") {
+    router.post("/create", async (req, res) => {
+        // res.send("It's working in the create section of owner router")
+        let owner = await ownerModel.find();
+        if(owner.length > 0) {
+            return res
+            .status(500)
+            .send("You have not the permission to create a owner");
+        }
+
+        let {fullname, email, password} = req.body;
+        let createdOwner = await ownerModel.create({
+            fullname,
+            email,
+            password
+        })
+        res.status(201).send(createdOwner)
+    })
+}
+
 
 router.get("/", (req, res) => {
     res.send("hey we send that")
 })
+
+
 
 module.exports = router;
